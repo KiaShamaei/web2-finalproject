7447f7a0f765632857b3a703f6a1dc0e90d2eb12
kiarash.shamaii <kiarash.shamaii@gmail.com>
2020-06-12 17:08:24 +0430

    last edit on pc

---
+++ js/jqs.js
@@ -0,0 +1,66 @@
+/** @format */
+
+$(window).on("load", function () {
+  $(window)
+    .scroll(function () {
+      var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+      $(".hidelement").each(function () {
+        var objectBottom = $(this).offset().top + $(this).outerHeight();
+        if (objectBottom < windowBottom) {
+          if ($(this).css("opacity") == 0) {
+            $(this).fadeTo(500, 1);
+          }
+        } else {
+          if ($(this).css("opacity") == 1) {
+            $(this).fadeTo(500, 0);
+          }
+        }
+      });
+    })
+    .scroll(); //invoke scroll-handler on page-load
+});
+// gallery function
+// $(function () {
+//   var selectedClass = "";
+//   $(".filter").click(function () {
+//     selectedClass = $(this).attr("data-rel");
+//     if ((selectedClass = 4)) {
+//       $(".all").show("slow");
+//     } else if ((selectedClass = 3)) {
+//       $(".pro1,.pro2").hide("slow");
+//     } else if ((selectedClass = 2)) {
+//       $(".pro2,.pro3").hide("slow");
+//     } else {
+//       $(".pro1,.pro3").hide("slow");
+//     }
+//   });
+// });
+$(document).ready(function () {
+  $(".all-filter").click(function () {
+    $(".all").show("slow");
+  });
+});
+$(document).ready(function () {
+  $(".filter-pro").click(function () {
+    $("#pro").toggleClass("text-info");
+    $("#new,#all,#free").removeClass("text-info");
+    $(".all").show("slow");
+    $(".pro").fadeOut("slow");
+  });
+});
+$(document).ready(function () {
+  $(".filter-new").click(function () {
+    $("#new").toggleClass("text-info");
+    $("#free,#pro,#all").removeClass("text-info");
+    $(".all").show("slow");
+    $(".new").hide("slow");
+  });
+});
+$(document).ready(function () {
+  $(".filter-free").click(function () {
+    $("#free").toggleClass("text-info");
+    $("#all,#pro,#new").removeClass("text-info");
+    $(".all").show("slow");
+    $(".free").fadeOut("slow");
+  });
+});
